45e3f2c5aac271c35ef875017624a3beb62d198b
Aria <calixto.hdz8malu@gmail.com>
2024-11-27 17:16:25 -0600

    Clases-carrito

---
+++ ProyectoUnida4/Controllers/CarritoController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProyectoUnida4.Models;
+using System.Text.Json.Serialization;
+
+public class CarritoController : Controller
+{
+    private List<Curso> _articulosCarrito;
+
+    public CarritoController()
+    {
+        _articulosCarrito = new List<Curso>();
+    }
+
+    [HttpPost]
+    public IActionResult AgregarCurso([FromBody] Curso curso)
+    {
+        var cursoExistente = _articulosCarrito.FirstOrDefault(c => c.Id == curso.Id);
+
+        if (cursoExistente != null)
+        {
+            cursoExistente.Cantidad++;
+        }
+        else
+        {
+            curso.Cantidad = 1;
+            _articulosCarrito.Add(curso);
+        }
+
+        SincronizarStorage();
+        return Json(_articulosCarrito);
+    }
+
+    [HttpPost]
+    public IActionResult EliminarCurso([FromBody] string cursoId)
+    {
+        var curso = _articulosCarrito.FirstOrDefault(c => c.Id == cursoId);
+
+        if (curso != null)
+        {
+            _articulosCarrito.Remove(curso);
+        }
+
+        SincronizarStorage();
+        return Json(_articulosCarrito);
+    }
+
+    [HttpPost]
+    public IActionResult VaciarCarrito()
+    {
+        _articulosCarrito.Clear();
+        SincronizarStorage();
+        return Json(_articulosCarrito);
+    }
+
+    [HttpGet]
+    public IActionResult ObtenerCarrito()
+    {
+        var carrito = HttpContext.Session.GetString("Carrito");
+
+        if (!string.IsNullOrEmpty(carrito))
+        {
+            _articulosCarrito = JsonConvert.DeserializeObject<List<Curso>>(carrito);
+        }
+
+        return Json(_articulosCarrito);
+    }
+
+    private void SincronizarStorage()
+    {
+        HttpContext.Session.SetString("Carrito", JsonConvert.SerializeObject(_articulosCarrito));
+    }
+}
+
---
+++ ProyectoUnida4/Models/Curso.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoUnida4.Models
+{
+    public class Curso
+    {
+        public string Imagen { get; set; }
+        public string Titulo { get; set; }
+        public string Precio { get; set; }
+        public string Id { get; set; }
+        public int Cantidad { get; set; }
+
+    }
+
+}
